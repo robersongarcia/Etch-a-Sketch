c65fcbcb32ed2d62b26837730683b0d54c507d9b
Roberson Garcia <roberson.gar12@gmail.com>
2022-10-02 22:22:28 -0400

    all functions ready

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const $container = document.querySelector("#container");
 const $rowsInput = document.querySelector("#rows");
 const $columnsInput = document.querySelector("#columns");
 const $colorPicker = document.querySelector("#colorPicker");
+const $clear = document.querySelector("#clear");
 
 function createSquares(rows = 16,columns = 16){
     let child = $container.lastElementChild; 
@@ -12,8 +13,8 @@ function createSquares(rows = 16,columns = 16){
 
     numOfSquares = rows * columns;
     const $fragment = document.createDocumentFragment();
-    const width = (800/rows)+"px";
-    const height = (800/columns)+"px";
+    const width = (800/columns)+"px";
+    const height = (800/rows)+"px";
     
 
     for(let i = 0;i < numOfSquares; i++){        
@@ -25,6 +26,7 @@ function createSquares(rows = 16,columns = 16){
     }
 
     $container.append($fragment);
+    loadSquares();
 }
 
 $rowsInput.value='16';
@@ -41,4 +43,18 @@ $columnsInput.addEventListener("change",() => {
 })
 
 
+$clear.addEventListener("click",()=>{
+    createSquares(parseInt($rowsInput.value),parseInt($columnsInput.value));
+})
 
+
+function loadSquares(){
+    const $squares = document.querySelectorAll(".square");
+
+    $squares.forEach(square => {
+        square.addEventListener("click",e =>{
+            e.target.style.backgroundColor = $colorPicker.value;        
+        })
+    })
+
+}
